8597a17f3fdf9deb2741253b3c8f34565391abd6
Kenny Dang <ynneknd@gmail.com>
2023-02-28 04:13:57 -0500

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/knd-v2/",
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
 });
